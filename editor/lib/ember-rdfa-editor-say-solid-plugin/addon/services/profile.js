33f1e886d9b346d140cb9392440337959342821e
Sitt Min Oo <sittminoo1995145@gmail.com>
2020-07-14 10:59:26 +0200

    init editing cards

---
+++ editor/lib/ember-rdfa-editor-say-solid-plugin/addon/services/profile.js
@@ -0,0 +1,7 @@
+import Service from '@ember/service';
+import normalizeLocation from '../utils/normalize-location';
+
+export default class ProfileService extends Service {
+ 
+
+}
---
+++ editor/lib/ember-rdfa-editor-say-solid-plugin/app/services/rdfa-editor-say-solid-edit-plugin.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-rdfa-editor-say-solid-plugin/services/rdfa-editor-say-solid-edit-plugin';
