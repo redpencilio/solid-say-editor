882d6e40dd78b6eabfb51ede267cad511ca4580c
Jonathan Poelman <jonathan.poelman@ugent.be>
2020-07-14 13:50:49 +0200

    added a profile service

--- editor/lib/ember-rdfa-editor-say-solid-plugin/addon/components/editor-plugins/say-solid-fetch-card.js
+++ editor/lib/ember-rdfa-editor-say-solid-plugin/addon/components/editor-plugins/say-solid-fetch-card.js
@@ -16,24 +16,19 @@ const { Fetcher, namedNode } = rdflib;
 export default class SaySolidFetchCard extends Component {
   @service  auth;
   @service("rdf-store") store;
-
-  async getProfileInfo(){
-    const graph = this.store.store.graph;
-    const me = graph.sym(this.auth.webId);
-    const fetcher = new Fetcher(graph);
-    await fetcher.load(me);
-    return this.store.create('solid/person', me, { defaultGraph: me.doc() } );
-  }
+  @service profile;
 
   @action
   async insert() {
-    const me = await this.getProfileInfo();
+    if(this.profile.me === null){
+      await this.profile.fetchProfileInfo();
+    }
     const info = this.args.info;
     info.hintsRegistry.removeHintsAtLocation( info.location, info.hrId, "say-solid-scope");
     const mappedLocation = info.hintsRegistry.updateLocationToCurrentIndex(info.hrId, info.location);
     const selection = info.editor.selectHighlight( mappedLocation );
     info.editor.update( selection, {
-      set: { innerHTML: `<a href=${this.auth.webId} property="foaf:name">${me.name}</a>` }
+      set: { innerHTML: `<a href=${this.auth.webId} property="foaf:name">${this.profile.me.name}</a>` }
     });
   }
 
