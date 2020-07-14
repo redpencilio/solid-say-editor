062cbb09f3fa14e8bae3b6316c25372c0b39165c
Jonathan Poelman <jonathan.poelman@ugent.be>
2020-07-14 17:28:38 +0200

    testing toggle

---
+++ editor/lib/ember-rdfa-editor-say-solid-plugin/addon/components/file-list.js
@@ -0,0 +1,10 @@
+import Component from '@glimmer/component';
+
+export default class FileListComponent extends Component {
+  
+  
+    @action
+    toggle(){
+        console.log("test");
+    }
+  }
