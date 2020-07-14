c21432aa62582d6e9987f28c18b38e75c0a973fc
Sitt Min Oo <sittminoo1995145@gmail.com>
2020-07-14 12:31:37 +0200

    remove unused method

--- editor/lib/ember-rdfa-editor-say-solid-plugin/addon/components/editor-plugins/say-solid-edit-card.js
+++ editor/lib/ember-rdfa-editor-say-solid-plugin/addon/components/editor-plugins/say-solid-edit-card.js
@@ -8,18 +8,5 @@ import SolidPersonModel from '../../models/solid/person';
 const { Fetcher, namedNode } = rdflib;
 
 export default class EditorPluginsSaySolidEditCardComponent extends Component {
-    
-  
-    @action
-    async insert() {
-      
-      const me = await this.getProfileInfo();
-      const info = this.args.info;
-      info.hintsRegistry.removeHintsAtLocation( info.location, info.hrId, "say-solid-scope");
-      const mappedLocation = info.hintsRegistry.updateLocationToCurrentIndex(info.hrId, info.location);
-      const selection = info.editor.selectHighlight( mappedLocation );
-      info.editor.update( selection, {
-        set: { innerHTML: `<a href=${this.auth.webId} property="foaf:name">${me.name}</a>` }
-      });
-    }
+
 }
