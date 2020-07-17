d1028d36a8255957e5013c348d5f58f3d66ce9a1
Sitt Min Oo <sittminoo1995145@gmail.com>
2020-07-17 11:27:39 +0200

    generic rdfa model

---
+++ editor/lib/ember-rdfa-editor-say-solid-plugin/addon/models/rdfa-semantic-model.js
@@ -0,0 +1,14 @@
+import SemanticModel, { property, string, integer, term, solid, rdfType } from 'solid-addon/models/semantic-model';
+
+
+
+export default class RdfaSemanticModel extends SemanticModel {
+
+    toRDFa() {
+        return undefined; 
+    }
+
+    fromRDFa(rdfa){
+        return undefined; 
+    }
+}
