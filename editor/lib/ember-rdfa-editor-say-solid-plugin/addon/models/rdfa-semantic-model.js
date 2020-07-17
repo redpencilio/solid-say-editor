15577f988b127f3932f20241dd915230dd0cd1cf
Sitt Min Oo <sittminoo1995145@gmail.com>
2020-07-17 13:45:24 +0200

    finish toRDFa

--- editor/lib/ember-rdfa-editor-say-solid-plugin/addon/models/rdfa-semantic-model.js
+++ editor/lib/ember-rdfa-editor-say-solid-plugin/addon/models/rdfa-semantic-model.js
@@ -4,23 +4,52 @@ import SemanticModel, { property, string, integer, term, solid, rdfType } from '
 
 export default class RdfaSemanticModel extends SemanticModel {
 
-    isRelevantContext(rdfa){
-        return false; 
-    }
-    
-    toRDFa() {
-        console.log("RdfaSemanticModel"); 
-        for (let attr in this.attributes){
-         console.log(attr);
-        }
-        return undefined; 
+    isRelevantContext(rdfa) {
+        return false;
     }
 
-    fromRDFa(rdfa){
-        if (this.isRelevantContext(rdfa)){
-            /** Translate to model */
-            return undefined; 
+    getPredObj(attr) {
+        let attrDef = this.attributeDefinitions[attr];
+
+        let property = undefined;
+        let type = undefined;
+        if (attrDef.predicate) {
+            property = attrDef.predicate.value;
+            type = attrDef.type;
+        } else {
+
+            property = attrDef.ns(attr).value;
+            type = attrDef.type;
         }
-        return undefined; 
+
+        return { property, type, value: this[attr].value ? this[attr].value : this[attr] };
+    }
+
+    generateRDFaTag({ property, type, value }) {
+        let content = type === "term" ? `href="${value}"` : `content="${value}"`;
+
+
+        return `<span property="${property}" ${content}/>`;
+    }
+
+    toRDFa() {
+        let start = `<span about="${this.uri}" typeof="${this.rdfType.value}">`;
+        let stack = [start];
+        for (let attr of this.attributes) {
+
+            let rdfaTag = this.generateRDFaTag(this.getPredObj(attr));
+            stack.push(rdfaTag);
+        }
+
+        stack.push("</span>");
+        return stack.join("\n");
+    }
+
+    fromRDFa(rdfa) {
+        if (this.isRelevantContext(rdfa)) {
+            /** Translate to model */
+            return undefined;
+        }
+        return undefined;
     }
 }
