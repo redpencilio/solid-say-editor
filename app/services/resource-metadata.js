3bc3ff62009ead0664c69591cc4d9392c878aab7
Jonathan Poelman <jonathan.poelman@ugent.be>
2020-08-04 12:51:45 +0200

    first version with annotations work

---
+++ app/services/resource-metadata.js
@@ -0,0 +1,17 @@
+import ResourceMetadataService from '@lblod/ember-rdfa-editor/services/resource-metadata';
+import { inject as service } from '@ember/service';
+import rdflib from 'ember-rdflib';
+const RDFS = rdflib.Namespace("http://www.w3.org/2000/01/rdf-schema#");
+
+export default class SolidResourceMetadataService extends ResourceMetadataService {
+
+    @service rdfaCommunicator;
+    async fetch(uri){
+        let resource = rdflib.sym(uri);
+        let metadata = await this.rdfaCommunicator.fetchResourceMetaData(resource);
+        return {
+            label: metadata[RDFS("label").value],
+            comment: metadata[RDFS("comment").value]
+        }
+    }
+}
